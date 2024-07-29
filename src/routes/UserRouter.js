cc5c0b0ae6d95e1d9d155d1c27313810c3ad6ca5
Paolo Cisneros <50997064+Tenfur@users.noreply.github.com>
2024-07-28 23:50:18 -0500

    test: endpoints

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -58,6 +58,8 @@ const userRouter = (db) => {
     
 
     router.post("/login", async (req, res) => {
+        console.log("body:", req.body)
+        console.log("db:", db)
         try{
             const result =  await userService.validateUser(req.body)
             res.status(200).json(result)
